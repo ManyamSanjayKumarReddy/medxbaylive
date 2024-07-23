93004930f42c0a091d84919fa2a24a211c1f6a1b
Loga priya Eswaran <logapriyaeswaran@Logas-MacBook-Air.local>
2024-07-23 11:28:16 +0530

    added responsivenss

--- src/MiddlePart.jsx
+++ src/MiddlePart.jsx
@@ -1,19 +1,26 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCircleCheck } from '@fortawesome/free-solid-svg-icons'
 import maledoc3 from './assests/img/maledoc3.png'
-import femaledoc3 from './assests/img/femaledoc3.png'
 import doctorProfile1 from './assests/img/Ellipse-30.png'
 import doctorProfile2 from './assests/img/doctor-profile-2.png'
 import doctorProfile3 from './assests/img/Ellipse-29.png'
 import doctorProfile4 from './assests/img/doctor-profile-4.png'
 import smallbar from './assests/img/smallbar.png'
 import largebar from './assests/img/largebar.png'
-import xImage from './assests/img/x-image.png'
 import './assests/MiddlePart.css'
-import { Card } from 'react-bootstrap'
+import { Card, Form, Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import yunche3 from '../src/assests/img/yunche2.png'
+import midyunche from '../src/assests/img/midyunche.png'
 
 const MiddlePart = () => {
+  const handleShowPopup = () => {
+    window.location.href = 'https://mxb-providerslaunch.zoholandingpage.com/patients/';
+  };
+  const handleShowPopupProviders = () => {
+    window.location.href = 'https://mxb-providerslaunch.zoholandingpage.com/zoho-marketing-automation-workspace/Prelaunch%20-%20Providers/';
+  };
 
   useEffect(() => {
     const handleScroll = () => {
@@ -39,7 +46,7 @@ const MiddlePart = () => {
 
     window.addEventListener('scroll', handleScroll);
 
-    // Clean up on component unmount
+
     return () => {
       window.removeEventListener('scroll', handleScroll);
     };
@@ -48,29 +55,46 @@ const MiddlePart = () => {
     <>
     <div className='main-section'>
       <div className="x-image1">
-        <img src={xImage} alt="X-Image" className="overlay" />
+        {/* <img src={xImage} alt="X-Image" className="overlay" /> */}
       </div>
       <div className='container mid-container'>
         <div className="row mt-5 d-flex flex-column-reverse flex-md-row">
           <div className="col-12 col-md-6 col-first pt-lg-5">
-            <div className="heading">LOREM ISPUM</div>
-            <div className="main-heading">Find Your Ideal Provider</div>
-            <div className="description">Whether you're seeking traditional medical advice or exploring modern and holistic treatment options, our directory connects you with healthcare providers who specialize in your specific condition or area of concern. From local specialists to top-rated clinics, find the right provider near you with ease.</div>
+            {/* <div className="heading">LOREM ISPUM</div> */}
+            <div className="main-heading">
+  <span className="highlight">Where Providers</span> 
+  <span className="secondary-text"> Connect, Earn, and Grow</span>
+</div>        
+    <div className="description"></div>
             <div className="check-list my-5">
               <div className='list d-flex'>
-                <FontAwesomeIcon className='mx-3 fa-size' icon={faCircleCheck} style={{ color: "#0167ff", }} />
-                <p >Lorem ipsum dolor sit amet consectetur.</p>
+                <FontAwesomeIcon className='mx-3 fa-size' icon={faCircleCheck} style={{ color: "#FF7F50", }} />
+                <p >Connect with Colleagues and Enhance Care</p>
               </div>
               <div className="list d-flex">
-                <FontAwesomeIcon className='mx-3 fa-size' icon={faCircleCheck} style={{ color: "#0167ff", }} />
-                <p>Lorem ipsum dolor sit amet consectetur.</p>
+                <FontAwesomeIcon className='mx-3 fa-size' icon={faCircleCheck} style={{ color: "#FF7F50", }} />
+                <p>Earn More and Expand Your Reach</p>
               </div>
               <div className="list d-flex">
-                <FontAwesomeIcon className='mx-3 fa-size' icon={faCircleCheck} style={{ color: "#0167ff", }} />
-                <p>Lorem ipsum dolor sit amet consectetur.</p>
+                <FontAwesomeIcon className='mx-3 fa-size' icon={faCircleCheck} style={{ color: "#FF7F50", }} />
+                <p>Grow Your Practice with Confidence</p>
+              </div>
+              <div className='list d-flex'>
+                <FontAwesomeIcon className='mx-3 fa-size' icon={faCircleCheck} style={{ color: "#FF7F50", }} />
+                <p >Automated Scheduling</p>
+              </div>
+              <div className='list d-flex'>
+                <FontAwesomeIcon className='mx-3 fa-size' icon={faCircleCheck} style={{ color: "#FF7F50", }} />
+                <p >Integrated Payment Processing</p>
+              </div>
+              <div className='list d-flex'>
+                <FontAwesomeIcon className='mx-3 fa-size' icon={faCircleCheck} style={{ color: "#FF7F50", }} />
+                <p >Patient Management System</p>
               </div>
             </div>
+
           </div>
+
           <div className="col-12 col-md-6">
             <div className=' d-flex flex-row justify-content-end'>
               <div className="overlay-container">
@@ -115,24 +139,24 @@ const MiddlePart = () => {
           <div className="col-12 col-md-6 mt-5 d-flex flex-row justify-content-start">
             <div className="overlay-container-2">
               <img
-                src={femaledoc3}
+                src={midyunche}
                 alt="overlay2"
                 className="overlay-image-2"
               />
             </div>
             <div className="reveal-2 reveal-card-2 " >
-              <Card className="reveal-profile-card card-3">
+            <Card className="reveal-profile-card card-3">
                 <Card.Body>
                   <Card.Title className="card-title-2">
                     <img src={doctorProfile4} alt="Doctor Profile" className="doctor-profile-img" />
-                    <span className="profile-name-2">Dr. Mamtha</span>
+                    <span className="profile-name-2">Mamtha</span>
                   </Card.Title>
                   <p className="bar-image-1 "><img src={largebar} alt="Large Bar" /></p>
                   <p className="bar-image-2"><img src={smallbar} alt="Small Bar" /></p>
-                  <Card.Text>Specialities</Card.Text>
+                  <Card.Text>Feedback</Card.Text>
                   <p className="bar-image-3 "><img src={largebar} alt="Large Bar" /></p>
                   <p className="bar-image-5 "><img src={largebar} alt="Large Bar" /></p>
-                  <Card.Text>Detailed profile</Card.Text>
+                  <Card.Text></Card.Text>
                   <p className="bar-image-4"><img src={largebar} alt="Large Bar" /></p>
                   <p className="bar-image-6"><img src={largebar} alt="Large Bar" /></p>
                 </Card.Body>
@@ -140,30 +164,48 @@ const MiddlePart = () => {
             </div>
           </div>
           <div className="col-12 col-md-6 col-second mt-5 pt-lg-5 d-flex flex-column justify-content-center">
-            <div className="heading">LOREM ISPUM</div>
-            <div className="main-heading">Own Your Health</div>
-            <div className="description">Dive into the healthcare scene and check out detailed profiles of providers, their specialties, blog posts, which insurance they accept, and their affiliated hospitals. It's more than just finding any provider; it's about finding the right one who really gets your health needs.</div>
+            {/* <div className="heading">LOREM ISPUM</div> */}
+            <div className="main-heading">
+  <span className="highlight">Become your</span> 
+  <span className="secondary-text"> Own Health Advocate</span>
+</div>    
+            <div className="description"></div>
             <div className="check-list my-5">
               <div className='list d-flex'>
-                <FontAwesomeIcon className='mx-3 fa-size' icon={faCircleCheck} style={{ color: "#0167ff", }} />
-                <p >Lorem ipsum dolor sit amet consectetur.</p>
+                <FontAwesomeIcon className='mx-3 fa-size' icon={faCircleCheck} style={{ color: "#FF7F50", }} />
+                <p>Real-Time Appointment Booking</p>
               </div>
               <div className="list d-flex">
-                <FontAwesomeIcon className='mx-3 fa-size' icon={faCircleCheck} style={{ color: "#0167ff", }} />
-                <p>Lorem ipsum dolor sit amet consectetur.</p>
+                <FontAwesomeIcon className='mx-3 fa-size' icon={faCircleCheck} style={{ color: "#FF7F50", }} />
+                <p>Telemedicine Services</p>
               </div>
-              <div className="list d-flex">
-                <FontAwesomeIcon className='mx-3 fa-size' icon={faCircleCheck} style={{ color: "#0167ff", }} />
-                <p>Lorem ipsum dolor sit amet consectetur.</p>
+              <div  className="list d-flex">
+                <FontAwesomeIcon className='mx-3 fa-size' icon={faCircleCheck} style={{ color: "#FF7F50", }} />
+                <p>Automated Reminders</p>
+              </div>
+              <div  className="list d-flex">
+                <FontAwesomeIcon className='mx-3 fa-size' icon={faCircleCheck} style={{ color: "#FF7F50", }} />
+                <p>Secure Messaging Platform</p>
+              </div>
+              <div  className="list d-flex">
+                <FontAwesomeIcon className='mx-3 fa-size' icon={faCircleCheck} style={{ color: "#FF7F50", }} />
+                <p>Trusted Provider Database</p>
+              </div>
+              <div  className="list d-flex">
+                <FontAwesomeIcon className='mx-3 fa-size' icon={faCircleCheck} style={{ color: "#FF7F50", }} />
+                <p>Vetted condition library</p>
               </div>
             </div>
+  
           </div>
           <div className="x-image2" style={{ display: 'flex', flexDirection: 'row-reverse' }}>
-            <img src={xImage} alt="X-Image" className="overlay" />
+            {/* <img src={xImage} alt="X-Image" className="overlay" /> */}
           </div>
         </div>
       </div>
     </div>
+
+
     </>
   )
 }
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,10 +1,8 @@
 import React, { useState, useEffect } from 'react';
-
 import './hero.css'; 
 import Navbar from '../Navbar/Navbar';
 import doctorImage from '../Assets/doctorImage.png';
 import { HiOutlineLocationMarker } from "react-icons/hi";
-
 import { IoMdSearch } from "react-icons/io";
 import { getNames, getCode } from 'country-list';
 import countries from 'i18n-iso-countries';
@@ -15,6 +13,10 @@ const Hero = () => {
     const [locations, setLocations] = useState([]);
     const [selectedLocation, setSelectedLocation] = useState(null);
 
+    const [showPopup, setShowPopup] = useState(false);
+
+    const handleShowPopup = () => setShowPopup(true);
+    const handleClosePopup = () => setShowPopup(false);
     useEffect(() => {
         const countriesList = getNames().map(country => {
             const alpha2Code = getCode(country);
@@ -35,14 +37,15 @@ const Hero = () => {
         setSelectedLocation(selectedOption);
     };
 
+
     return (
         <>  
            <div className='hero-head container-fluid'>
                 <Navbar/>
                 <div className='row hero-content align-items-center'>
-                <div className="col-md-6 col-12 hero-item">
-                        <h5>CERTIFIED DOCTORS</h5>
-                        <h1 className="hero-title">We Find <span>Best Doctors</span> <br /> Based On Your Condition</h1>
+                    <div className="col-md-6 col-12 hero-item">
+                        <h5>Be Your Own Health Advocate</h5>
+                        <h1 className="hero-title">Find the<span>  Best Providers</span> <br /> Based On Your Condition</h1>
                         <div className='search-box'>
                             <div className="custom-select-wrapper">
                                 <HiOutlineLocationMarker className="icon-loc-src"/>
@@ -63,32 +66,30 @@ const Hero = () => {
                                 <input type="text" className='search-input' placeholder="Search Doctors" />
                                 <div className='simple-line-small'></div>
                                 <div className="outer">
-                                    <button className="search-button">Find My Doctor</button>
+                                    <button className="search-button" onClick={handleShowPopup} >Find My Doctor</button>
                                 </div>
                             </div>
                         </div>
                     </div>
 
-
-
                     <div className="col-md-6 col-12 text-center">
-                      <div className='image-division'>
+                        <div className='image-division'>
                             <div className="ecg-container">
                                 <svg viewBox="0 0 100 50" className="ecg-line">
                                     <polyline className="outline" points="0,25 10,25 15,13 20,35 25,25 33,25 40,7 47,40 53,25 65,25 68,15 74,35 80,25 95,25" />
                                     <polyline className="line" points="0,25 10,25 15,13 20,35 25,25 33,25 40,7 47,40 53,25 65,25 68,15 74,35 80,25 100,25" />
                                 </svg>
                             </div>
-                        <img src={doctorImage} alt="Doctors" className="hero-image"/>
-                        <div className="layout"></div> 
-                    </div> 
+                            <img src={doctorImage} alt="Doctors" className="hero-image"/>
+                            <div className="layout"></div> 
+                        </div> 
                     </div>
                 </div>
-           </div> 
+           </div>
+
+
         </>
     );
 };
 
 export default Hero;
-
-
